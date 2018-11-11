475a7766cb71cb7c1877d9fba9e342c60e941b4a
Peter Kurfer <peter.kurfer@fh-rosenheim.de>
2018-11-11 18:49:31 +0100

    Updated directory structure for THRO

--- app/src/main/java/de/thro/inf/prg3/a07/MainActivity.java
+++ app/src/main/java/de/thro/inf/prg3/a07/MainActivity.java
@@ -2,16 +2,144 @@ package de.thro.inf.prg3.a07;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import de.thro.inf.prg3.a07.api.OpenMensaAPI;
+import de.thro.inf.prg3.a07.model.Meal;
+import de.thro.inf.prg3.a07.utils.MealsFilterUtility;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	private final SimpleDateFormat dateFormat;
+	private OpenMensaAPI openMensaAPI;
+	private ArrayAdapter<Meal> mealArrayAdapter;
+	private CheckBox vegetarianCheckbox;
 
-        // this will inflate the layout from res/layout/activity_main.xml
-        setContentView(R.layout.activity_main);
+	public MainActivity() {
+		dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+	}
 
-        // add your code here
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		/* create Retrofit instance to get a OpenMensaAPI proxy object */
+		Retrofit retrofit = new Retrofit.Builder()
+			/* no type adapters are required so the default GSON converter is fine */
+			.addConverterFactory(GsonConverterFactory.create())
+			/* retrieve OpenMensaAPI URL from resources */
+			.baseUrl(getResources().getString(R.string.openmensa_api_base_url))
+			.build();
+
+		openMensaAPI = retrofit.create(OpenMensaAPI.class);
+
+		// this will inflate the layout from res/layout/activity_main.xml
+		setContentView(R.layout.activity_main);
+
+		/* 'caching' the reference to the CheckBox */
+		vegetarianCheckbox = findViewById(R.id.vegetarianChBox);
+
+		/* create the ArrayAdapter without an given list instance */
+		mealArrayAdapter = new ArrayAdapter<>(
+			this,
+			R.layout.meal_entry
+		);
+
+		/* 'caching' the reference to the ListView */
+		final ListView mealsListView = findViewById(R.id.mealsListView);
+		mealsListView.setAdapter(mealArrayAdapter);
+
+		/* retrieving a reference to the refresh button */
+		Button button =  findViewById(R.id.refreshButton);
+
+		/* register click handler */
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				/* delegate call to helper method to reduce nesting */
+				onRefreshBtnClick();
+			}
+		});
+
+		/* register click handler to the CheckBox to filter whenever the CheckBox is checked or unchecked */
+		vegetarianCheckbox.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				/* delegate call to the same helper method as for the button click */
+				onRefreshBtnClick();
+			}
+		});
+	}
+
+	/**
+	 * Handle the click on the refresh button and changed state of the CheckBox
+	 */
+	private void onRefreshBtnClick() {
+		/* execute call to the OpenMensaAPI
+		 * note that the .enqueue(...) method is used instead of .execute() */
+		openMensaAPI.getMeals(dateFormat.format(new Date())).enqueue(new Callback<List<Meal>>() {
+			@Override
+			public void onResponse(Call<List<Meal>> call, Response<List<Meal>> response) {
+				/* delegate success handling to helper method to reduce nesting */
+				onApiSuccess(response);
+			}
+
+			@Override
+			public void onFailure(Call<List<Meal>> call, Throwable t) {
+				/* display an error message if call failed */
+				Toast.makeText(
+					MainActivity.this,
+					"An error occured while fetching meals",
+					Toast.LENGTH_LONG
+				).show();
+			}
+		});
+	}
+
+	/**
+	 * Helper method to handle the response of the OpenMensaAPI
+	 * @param response
+	 */
+	private void onApiSuccess(Response<List<Meal>> response) {
+		/* check if response code was 2xx */
+		if(response.isSuccessful()){
+			/* remove elements from the adapter */
+			mealArrayAdapter.clear();
+			/* unwrap the retrieved meals */
+			List<Meal> retrievedMeals = response.body();
+
+			/* Check if we should filter for vegetarian meals */
+			if(vegetarianCheckbox.isChecked()){
+				List<Meal> vegetarian = MealsFilterUtility.filterForVegetarian(retrievedMeals);
+
+				/* add all filtered meals to the adapter to display them in the view */
+				mealArrayAdapter.addAll(vegetarian);
+			}else {
+				/* add all retrieved meals to the adapter because we haven't to filter for anything */
+				mealArrayAdapter.addAll(retrievedMeals);
+			}
+		}else {
+			/* display an error message if response code was not one of 2xx */
+			Toast.makeText(
+				MainActivity.this,
+				"An error occured while fetching meals",
+				Toast.LENGTH_LONG
+			).show();
+		}
+	}
 }
--- app/src/test/java/de/thro/inf/prg3/a07/test/OpenMensaAPITests.java
+++ app/src/test/java/de/thro/inf/prg3/a07/test/OpenMensaAPITests.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.logging.Logger;
 
 import de.thro.inf.prg3.a07.api.OpenMensaAPI;
 import de.thro.inf.prg3.a07.model.Meal;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -21,45 +28,62 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  * Created by Peter Kurfer on 11/19/17.
  */
 
-public class OpenMensaAPITests {
+class OpenMensaAPITests {
 
-    private static final Logger logger = Logger.getLogger(OpenMensaAPITests.class.getName());
-    private OpenMensaAPI openMensaAPI;
+	private static final Logger logger = Logger.getLogger(OpenMensaAPITests.class.getName());
+	private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+	private OpenMensaAPI openMensaAPI;
 
-    @BeforeEach
-    public void setup() {
-        // use this to intercept all requests and output them to the logging facilities
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+	private static Date getUpcomingMondayDate() {
+		Calendar cal = Calendar.getInstance();
+		while (cal.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+			cal.add(Calendar.DAY_OF_MONTH, 1);
+		}
+		return cal.getTime();
+	}
 
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(interceptor)
-                .build();
+	@BeforeEach
+	void setup() {
+		// use this to intercept all requests and output them to the logging facilities
+		HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+		interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl("http://openmensa.org/api/v2/")
-                .client(client)
-                .build();
+		/* create a OkHttpClient and register the interceptor */
+		OkHttpClient client = new OkHttpClient.Builder()
+			.addInterceptor(interceptor)
+			.build();
 
-        openMensaAPI = retrofit.create(OpenMensaAPI.class);
-    }
+		/* create a Retrofit instance
+		 * register the default GSON factory
+		 * register the created client with HTTP interceptor */
+		Retrofit retrofit = new Retrofit.Builder()
+			.addConverterFactory(GsonConverterFactory.create())
+			.baseUrl("https://openmensa.org/api/v2/")
+			.client(client)
+			.build();
 
-    @Test
-    public void testGetMeals() throws IOException {
-        // TODO prepare call
+		/* retrieve a proxy object for the OpenMensaAPI interface */
+		openMensaAPI = retrofit.create(OpenMensaAPI.class);
+	}
 
-        // TODO execute the call synchronously
+	@Test
+	void testGetMeals() throws IOException {
+		/* create a call to get all meals of the current day */
+		Call<List<Meal>> mealsCall = openMensaAPI.getMeals(dateFormat.format(getUpcomingMondayDate()));
 
-        // TODO unwrap the body
-        List<Meal> meals = null;
+		/* execute the call synchronous */
+		Response<List<Meal>> mealsResponse = mealsCall.execute();
 
-        assertNotNull(meals);
-        assertNotEquals(0, meals.size());
+		/* unwrap the response */
+		List<Meal> meals = mealsResponse.body();
 
-        for(Meal m : meals){
-            logger.info(m.toString());
-        }
-    }
+		assertNotNull(meals);
+		assertNotEquals(0, meals.size());
+
+		/* display the results in the log of the test */
+		for (Meal m : meals) {
+			logger.info(m.toString());
+		}
+	}
 
 }
